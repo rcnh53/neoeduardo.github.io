a40baab97e153d828d203ce151880f82e81faafd
Neo <72762993+NeoEduardo@users.noreply.github.com>
2021-05-01 10:23:21 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -10,8 +10,6 @@ rm -rf temp
 mkdir temp
 curl "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt" > temp/GoodByeAds
 curl "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" > temp/StevenBlack
-curl "https://raw.githubusercontent.com/nickspaargaren/no-google/master/google-domains" > temp/NoGoogle
-curl "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt" | grep -v "^#" | sed "s/^/0.0.0.0 /g" > temp/Adguard
 cat temp/* >> hosts_new
 cat hosts_new | grep -v "^#" | grep -v "^!" | grep -v "^ "| grep -v "^\t" | grep -v "^white" | sort | uniq > hosts
 rm -f hosts_new
