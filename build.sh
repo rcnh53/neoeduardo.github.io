cd4a452936d6033259e8e0fab698d27ff668a86c
Neo <72762993+NeoEduardo@users.noreply.github.com>
2021-05-01 10:21:33 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -9,15 +9,10 @@ done
 rm -rf temp
 mkdir temp
 curl "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt" > temp/GoodByeAds
-curl "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-YouTube-AdBlock.txt" > temp/GoodByeAds-Youtube
-curl "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Xiaomi-Extension.txt" > temp/GoodByeAds-Xiaomi
-curl "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Spotify-AdBlock.txt" > temp/GoodByeAds-Spotify
 curl "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" > temp/StevenBlack
-curl "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt" > temp/anudeepND
-curl "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts" > temp/1
-curl "https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts" > temp/2
-curl "https://raw.githubusercontent.com/tiuxo/hosts/master/ads" > temp/3
-
+curl "https://raw.githubusercontent.com/nickspaargaren/no-google/master/google-domains" > temp/NoGoogle
+curl "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt" | grep -v "^#" | sed "s/^/0.0.0.0 /g" > temp/Adguard
 cat temp/* >> hosts_new
 cat hosts_new | grep -v "^#" | grep -v "^!" | grep -v "^ "| grep -v "^\t" | grep -v "^white" | sort | uniq > hosts
 rm -f hosts_new
+ 
