613a50bd836813af42daa10034ae3eb6ae24637f
Neo <72762993+NeoEduardo@users.noreply.github.com>
2021-05-14 12:31:15 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ rm -rf temp
 mkdir temp
 curl "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt" > temp/GoodByeAds
 curl "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" > temp/StevenBlack
+curl "https://adaway.org/hosts.txt" > temp/Adaway
+curl "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" > temp/Pgl
 cat temp/* >> hosts_new
 cat hosts_new | grep -v "^#" | grep -v "^!" | grep -v "^ "| grep -v "^\t" | grep -v "^white" | sort | uniq > hosts
 rm -f hosts_new
- 
