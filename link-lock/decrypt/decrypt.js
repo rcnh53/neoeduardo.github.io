4df9d8a13c6d7a122276e0bd29fcda7b9bde350c
Neo <72762993+NeoEduardo@users.noreply.github.com>
2021-05-23 16:46:09 +0000

    Update decrypt.js

--- link-lock/decrypt/decrypt.js
+++ link-lock/decrypt/decrypt.js
@@ -113,6 +113,6 @@ function onCopy(id) {
 function main() {
   if (window.location.hash) {
     document.querySelector("#encrypted-url").value =
-      `https://neo.monster/link-lock/${window.location.hash}`;
+      `https://neoeduardo.github.io/link-lock/${window.location.hash}`;
   }
 }
