7a33bdd8dd8526aeeef56e3f5ed34a09ab579698
Neo <72762993+NeoEduardo@users.noreply.github.com>
2021-05-23 16:47:33 +0000

    Update create.js

--- link-lock/create/create.js
+++ link-lock/create/create.js
@@ -118,13 +118,13 @@ async function onEncrypt() {
 
   const encrypted = await generateFragment(url, password, hint, useRandomSalt,
       useRandomIv);
-  const output = `https://neo.monster/link-lock/#${encrypted}`;
+  const output = `https://neoeduardo.github.io/link-lock/#${encrypted}`;
 
   document.querySelector("#output").value = output;
   highlight("output");
 
   // Adjust "Hidden Bookmark" link
-  document.querySelector("#bookmark").href = `https://neo.monster/link-lock/hidden/#${encrypted}`;
+  document.querySelector("#bookmark").href = `https://neoeduardo.github.io/link-lock/hidden/#${encrypted}`;
 
   // Adjust "Open in New Tab" link
   document.querySelector("#open").href = output;
